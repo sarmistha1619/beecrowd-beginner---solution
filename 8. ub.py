a3ec46c3abb1df5ea6cbc7a88b9879644dbb2291
Sarmistha Pal <sarmistha15-9306@diu.edu.bd>
2021-12-19 00:59:04 +0600

    Add files via upload

---
+++ 8. ub.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = float(input())
+d=b*c
+d = "{:.2f}".format(d)
+'''
+print(f"NUMBER = {a}")
+print(f"SALARY = U$ {d}:.2f")
+'''
+
+print("NUMBER =",a)
+print("SALARY = U$",d)
